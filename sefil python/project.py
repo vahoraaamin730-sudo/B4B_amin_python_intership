a7000e253fb0951c19e6b7bbd5310088fea40e92
vahoraaamin730-sudo <vhoraaamin730@gmail.com>
2026-07-16 14:47:50 +0530

    safil ni file c draw ma muki

--- sefil python/project.py
+++
@@ -1,7 +0,0 @@
-import pywhatkit as pw
-
-txt="""python is an interpreted high level general-purpose programing
-Its design pilosophy emphasizes code readability with its use of signi
-
-pw.text_to_handwriting("txt,"demo1.png",[0,0,138])
-print(" END ")
